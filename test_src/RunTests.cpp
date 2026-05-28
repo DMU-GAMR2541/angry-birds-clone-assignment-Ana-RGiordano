00e5fc136dfb44d35ce4296ca4ae48355f1da03f
AGiordano <P2861911@my365.dmu.ac.uk>
2026-05-28 03:26:46 +0100

    added pig fixture setup and teardown tests

--- test_src/RunTests.cpp
+++ test_src/RunTests.cpp
@@ -1,15 +1,22 @@
 #include <gtest/gtest.h>
 #include "Enemy.h"
 #include <memory>
+#include "Pig.h"
+#include <box2d/box2d.h>
+#include <SFML/Graphics.hpp>
 
 /// <summary>
 ///Taken from the GoogleTest primer. 
 /// </summary>
 
 // The fixture for testing class Foo.
+
+//enemy is now an abstract class so i adapted the fixture to use the pig class instead
 class EnemyTest : public testing::Test {
 public:
-    std::unique_ptr<Enemy> enemy;
+    std::unique_ptr<Pig> pig;
+    std::unique_ptr<b2World> world;
+    std::unique_ptr<sf::RenderWindow> window;
 protected:
     // You can remove any or all of the following functions if their bodies would
     // be empty.
@@ -30,12 +37,20 @@ protected:
         // Code here will be called immediately after the constructor (right
         // before each test).
         //enemy = std::make_unique<Enemy>(50); // All enemnies in this test suite start with 50 HP.
+        world = std::make_unique<b2World>(b2Vec2(0.0f, 9.8f));
+        window = std::make_unique<sf::RenderWindow>();
+        pig = std::make_unique<Pig>(b2Vec2(100.0f / 30.0f, 500.0f / 30.0f), *world, "C:/Users/abeat/source/repos/angry-birds-clone-assignment-Ana-RGiordano/assets/Ang_Birds/sprite_1.png", *window,
+            50, 15.0f, 30.0f, 57, 57);
+
 
     }
 
     void TearDown() override {
         // Code here will be called immediately after each test (right
         // before the destructor).
+        pig.reset();
+        window.reset();
+        world.reset();
     }
 
 
@@ -56,7 +71,7 @@ protected:
 //}
 
 
-//initial assertion test
+//binary comparison initial assertion test 
 TEST(Assertions, BasicNonFatalAssertions) {
     EXPECT_EQ(100, 100);
     EXPECT_GT(100, 50);
@@ -64,7 +79,7 @@ TEST(Assertions, BasicNonFatalAssertions) {
     SUCCEED() << "Basic non-fatal assertions passed";
 }
 
-//fatal assertion
+//fatal assertions
 TEST(Assertions, FatalAssertions) {
     int health = 100;
     ASSERT_EQ(health, 100);
@@ -82,6 +97,23 @@ TEST(Assertions, TypeComparisons) {
     EXPECT_EQ(birdType, "Red");
     EXPECT_NE(health, 50);
     EXPECT_NE(birdType, "Fast");
+}
+//fxiture tests
+
+//tests starting health
+TEST_F(EnemyTest, PigHealthStartsAtExpectedValue) {
+    EXPECT_EQ(pig->getHealth(), 50);
+}
+
+// tests health reduction
+TEST_F(EnemyTest, PigHealthCanBeReduced) {
+    pig->takeHit(20);
+    EXPECT_EQ(pig->getHealth(), 30);
+}
+// tests if the pig is popped after lethal damage
+TEST_F(EnemyTest, LethalDamagePopsPig) {
+    pig->takeHit(60);
+    EXPECT_TRUE(pig->checkIfPopped());
 }
 
 int main(int argc, char** argv) {
