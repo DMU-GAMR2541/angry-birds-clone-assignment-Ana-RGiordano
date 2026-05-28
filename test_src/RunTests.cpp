e90041b6d46aa183e8e23daf30d2034fb63d283d
AGiordano <P2861911@my365.dmu.ac.uk>
2026-05-28 02:35:10 +0100

    added initial assertion test

--- test_src/RunTests.cpp
+++ test_src/RunTests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "Enemy.h"
+#include <memory>
 
 /// <summary>
 ///Taken from the GoogleTest primer. 
@@ -15,7 +16,7 @@ protected:
 
     EnemyTest() {
         // You can do set-up work for each test here.
-                    
+
     }
 
     ~EnemyTest() override {
@@ -28,8 +29,8 @@ protected:
     void SetUp() override {
         // Code here will be called immediately after the constructor (right
         // before each test).
-        enemy = std::make_unique<Enemy>(50); // All enemnies in this test suite start with 50 HP.
-                    
+        //enemy = std::make_unique<Enemy>(50); // All enemnies in this test suite start with 50 HP.
+
     }
 
     void TearDown() override {
@@ -40,69 +41,24 @@ protected:
 
 };
 
-class ParamTest : public::testing::TestWithParam<int> {
-protected:
-    ParamTest() = default;
-    ~ParamTest() = default;
-
-    void SetUp()override {
-    }
-};
-
-TEST_P(ParamTest, SimpleTest) {
-    int i_test = GetParam();
-    std::cout << "Param value::" << i_test << std::endl;
-    EXPECT_GT(i_test, 1);
-}
-
-INSTANTIATE_TEST_SUITE_P(
-    Simple,
-    ParamTest, ::testing::Values(1, 2, 3, 4, 5)
-);
-
-
 //A single test, not a fixture. No setup is called.
 //TEST(Enemy, First_test) {
-  //  Enemy (100);
-    //std::cout << e.getHealth() << " " << 100 << std::endl;
+    //Enemy e(100);
     //EXPECT_GT(e.getHealth(), 100);
+    //SUCCEED() << "Test test passed";
 //}
 
-TEST_F(EnemyTest, LethalDamagePopsPig) {
-    enemy->takeDamage(60);
-    EXPECT_TRUE(enemy->checkIfPopped());
+TEST(Assertions, BasicNonFatalAssertions) {
+    EXPECT_EQ(100, 100);
+    EXPECT_GT(100, 50);
+    EXPECT_LT(50, 100);
+    SUCCEED() << "Basic non-fatal assertions passed";
 }
 
-TEST_F(EnemyTest, NegativeDamageDoesNothing) {
-    int initialHealth = enemy->getHealth();
-    enemy->takeDamage(-10);
-    EXPECT_EQ(enemy->getHealth(), initialHealth);
-    EXPECT_FALSE(enemy->checkIfPopped());
-} // checks if negative dmaage does nothing
-
-TEST(Assertions, NonFatalEquality) {
-    EXPECT_EQ(5, 5);
-    EXPECT_FLOAT_EQ(1.5f, 1.5f);
-    EXPECT_STREQ("hello", "hello");
-} // tests that values are equal
-
-TEST(Assertions, NonFatalEquality) {
-    EXPECT_NE(5, 10);
-    EXPECT_STREQ("hello", "world");
-} // check values are not equal
-
-TEST(Assertions, NonFatalComparisons) {
-    EXPECT_GT(10, 5);
-    EXPECT_LT(5, 10);
-    EXPECT_GE(10, 10);
-    EXPECT_LE(5, 5);
-}// testing values that are <,<,>=,<=
-
-TEST(NewTest, test) {
-    std::string str = "Check";
-    const char* c = str.c_str();
-    EXPECT_STREQ(c, "Check");
-}
+//TEST_F(EnemyTest, LethalDamagePopsPig) {
+   // enemy->takeDamage(60);
+    //EXPECT_TRUE(enemy->checkIfPopped());
+//}
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
