6a0ee8663c71700b538ec32123c33b2a88d2b0b8
AGiordano <P2861911@my365.dmu.ac.uk>
2026-06-04 23:14:28 +0100

    created UI class

--- include/UI.h
+++ include/UI.h
@@ -1,3 +1,18 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
+class UI {
+private:
+	sf::Font font;
+	sf::Text titleText;
+	sf::RectangleShape loadingBarBackground;
+	sf::RectangleShape loadingBarFill;
+
+public:
+	UI();
+
+	void update(float progress);
+	void draw(sf::RenderWindow& window);
+
+
+};
